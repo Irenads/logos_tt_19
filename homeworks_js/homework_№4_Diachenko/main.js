2fb199682b2b636c67de3b741e912901523a1283
Irenads <irenadiachenko@gmail.com>
2020-04-30 17:00:32 +0300

    second decision

--- homeworks_js/homework_№4_Diachenko/main.js
+++ homeworks_js/homework_№4_Diachenko/main.js
@@ -1,24 +1,39 @@
 
-let password;
-let chek=5;
+// let password;
+// let chek=5;
 
 
-while(password != '1111'){
-    password=prompt('Ведіть пароль');
-    chek--
+// while(password != '1111'){
+//     password=prompt('Ведіть пароль');
+//     chek--
 
-    if(password=='1111'){
-        alert('Запрошуємо на сайт')
-        continue
-    }
-    else{
-        alert('У Вас залишилось ' + chek+ ' спроб') 
-    }
+//     if(password=='1111'){
+//         alert('Запрошуємо на сайт')
+//         continue
+//     }
+//     else{
+//         alert('У Вас залишилось ' + chek+ ' спроб') 
+//     }
 
-   if(chek==0){
+//    if(chek==1){
        
-    alert('Будь ласка, спробуйте пізніше')
-    password='1111'
-   }
+//     alert('Будь ласка, спробуйте пізніше')
+//     password='1111'
+//    }
     
+// }
+
+let ask = prompt('Ведіть пароль');
+let n = 5;
+while (ask !== '1111' && n >1) {
+    n--
+    alert('У Вас залишилось ' + n+ ' спроб')
+    ask = prompt('Ведіть пароль');   
+}
+
+if (ask == '1111') {
+    alert('Запрошуємо на сайт')
+}
+else {
+    alert('Будь ласка, спробуйте пізніше')
 }
